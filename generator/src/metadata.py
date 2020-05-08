68956a032e7630bce242cfd2c0145d588cd99d30
Jesus Gazol <jgaz@equinor.com>
2020-05-08 12:11:33 +0200

    Tests covering symbol mangling

--- generator/src/metadata.py
+++ generator/src/metadata.py
@@ -6,6 +6,11 @@ import pandas as pd
 from config import METADATA_PATH
 
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 @dataclass
 class SymbolData:
     name: str
@@ -16,6 +21,15 @@ class SymbolData:
 class SymbolStorage:
     symbols_metadata_file = os.path.join(METADATA_PATH, "symbols.pickle")
     data: pd.DataFrame = None
+
+    def __init__(self):
+        try:
+            self._read()
+        except Exception as e:
+            logger.error(
+                "Cannot read the source file for symbols, check that you have generated it"
+            )
+            raise e
 
     def save(self, symbols: List[Tuple[str, str, str]]):
         with open(self.symbols_metadata_file, "wb") as f_out:
@@ -30,16 +44,18 @@ class SymbolStorage:
             )
         return self.data
 
+    def _pandas_to_symbol_data(self, df: pd.DataFrame) -> List[SymbolData]:
+        symbol_list: List[SymbolData] = []
+        for item in list(df.values):
+            symbol_list.append(SymbolData(*item))
+        return symbol_list
+
     def get_families(self) -> List[str]:
-        families: List[str] = []
-        self._read()
-        return families
+        return list(self.data.family.unique())
 
     def get_symbols_by_family(self, symbol_family: str) -> List[SymbolData]:
-        symbols: List[SymbolData] = []
-        self._read()
-        return symbols
+        df_filtered = self.data[self.data["family"] == symbol_family]
+        return self._pandas_to_symbol_data(df_filtered)
 
     def get_dataframe(self):
-        self._read()
         return self.data
--- generator/tests/unit/test_metadata.py
+++ generator/tests/unit/test_metadata.py
@@ -1,4 +1,7 @@
 import logging
+
+import pandas as pd
+
 from metadata import SymbolStorage
 
 logger = logging.getLogger(__name__)
@@ -9,3 +12,24 @@ class TestSymbolStorage:
         ss = SymbolStorage()
         df = ss.get_dataframe()
         assert len(df.name) > 0
+
+    def test_get_families(self):
+        ss = SymbolStorage()
+        families = ss.get_families()
+        assert len(families) > 0
+        assert families[0] != ""
+
+    def test_get_symbols_by_family(self):
+        ss = SymbolStorage()
+        families = ss.get_families()
+        symbols = ss.get_symbols_by_family(families[0])
+        assert len(symbols) > 0
+
+    def test_pandas_to_symbol_data(self):
+        df = pd.DataFrame([["a", "b", "c"]], columns=["name", "family", "description"])
+        ss = SymbolStorage()
+        symbol_list = ss._pandas_to_symbol_data(df)
+        assert len(symbol_list) == 1
+        assert symbol_list[0].name == "a"
+        assert symbol_list[0].family == "b"
+        assert symbol_list[0].description == "c"
